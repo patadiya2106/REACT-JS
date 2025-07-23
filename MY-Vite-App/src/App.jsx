7489871f6b41d83d32bc942ad740b3bf80fa073d
Rahul Patadiya <rahulpatadiya07@gmail.com>
2025-07-23 19:40:40 +0530

    My First React App

---
+++ MY-Vite-App/src/App.jsx
@@ -0,0 +1,13 @@
+  
+function App() {
+  return (  
+    <>
+      <h1>My Introduction</h1>
+      <p>My name is Rahul.</p>
+      <p>I am a MERN Developer.</p>
+        
+    </>
+  )
+}
+
+export default App
---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+       
+function App() {
+  return (
+    <>
+      <h1>My Introducation</h1>
+      <p>My name is Rahul.</p>
+      <p>I am a MERN Developer.</p>
+      <p>I am from India.</p>
+  </>
+  );
+}
+
+export default App;
